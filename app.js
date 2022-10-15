25506fb89422c07bca85aedc3a26bddbcf5fd1fa
yebrai <ivansevv@gmail.com>
2022-10-15 18:28:39 +0200

    added homepage

--- app.js
+++ app.js
@@ -17,11 +17,15 @@ const spotifyApi = new SpotifyWebApi({
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET
   });
+  
+  spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => /*console.log('Something went wrong when retrieving an access token', error)*/ error);
 
-// Our routes go here:
-spotifyApi
-  .clientCredentialsGrant()
-  .then(data => spotifyApi.setAccessToken(data.body['access_token']))
-  .catch(error => console.log('Something went wrong when retrieving an access token', error));
+  // Our routes go here:
+  app.get("/", (req, res, next) => {
+    res.render("index.hbs")
+  })
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
